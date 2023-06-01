f17dcf0629bdc0ed86e43647341ea0664c81a053
masayuki <sg.pythondev@gmail.com>
2023-06-01 10:15:24 +0300

    take over all cardboards to admin page

--- src/main/java/com/material/pmsa/MainController.java
+++ src/main/java/com/material/pmsa/MainController.java
@@ -3,12 +3,23 @@ package com.material.pmsa;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.material.pmsa.cardboard.Cardboard;
+import com.material.pmsa.cardboard.CardboardRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.ui.ModelMap;
 
 
 @Controller
 public class MainController {
     
+    @Autowired
+    private CardboardRepository cardBoardRepository;
+
 	@GetMapping("/")
 	public String index(ModelMap model) {
         model.addAttribute("message", "Greetings from Spring Boot!");
@@ -17,6 +28,15 @@ public class MainController {
 
 	@GetMapping("/admin")
 	public String admin(ModelMap model) {
+		Iterable<Cardboard> allCardboards = cardBoardRepository.findAll();
+		ObjectMapper objectMapper = new ObjectMapper();
+		String jsonString = null;
+		try {
+			jsonString = objectMapper.writeValueAsString(allCardboards);
+		} catch(JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		model.addAttribute("allCardboards", jsonString);
 		return "admin/index";
 	}
 }
