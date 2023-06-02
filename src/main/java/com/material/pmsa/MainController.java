d15ead4395aabcf1430e359b8ed0b3443a1236ec
masayuki <sg.pythondev@gmail.com>
2023-06-02 03:45:46 +0300

    deny search

--- src/main/java/com/material/pmsa/MainController.java
+++ src/main/java/com/material/pmsa/MainController.java
@@ -1,7 +1,10 @@
 package com.material.pmsa;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.material.pmsa.cardboard.Cardboard;
 import com.material.pmsa.cardboard.CardboardRepository;
@@ -38,5 +41,22 @@ public class MainController {
 		// }
 		model.addAttribute("allCardboards", allCardboards);
 		return "admin/index";
+	}
+
+	@PostMapping("/select-optimal-board")
+	public @ResponseBody String selectOptimalBoard (
+		@RequestParam(required = true) Float length,
+		@RequestParam(required = true) Float width,
+		@RequestParam(required = true) Float height,
+		@RequestParam(required = true) Float weight,
+		@RequestParam(required = true) Float gap
+	) {
+		Iterable<Cardboard> allCardboards = cardBoardRepository.findAll();
+		String lengthStr = Float.toString(length);
+		String widthStr = Float.toString(width);
+		String heightStr = Float.toString(height);
+		String weightStr = Float.toString(weight);
+		String gapStr = Float.toString(gap);
+		return lengthStr + widthStr + heightStr + weightStr + gapStr;
 	}
 }
